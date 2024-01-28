879e604a4f1a79bb0718a79d3cf9006bc60fdf5e
Ka Lok (Rico) Lo <rico.kaloklo@gmail.com>
2024-01-28 20:10:23 +0100

    Implement plot_catalog()

--- lenscat/utils.py
+++ lenscat/utils.py
@@ -1,6 +1,7 @@
 import os
 import copy
 import re
+import numpy as np
 import astropy.units as u
 import requests
 
@@ -9,6 +10,8 @@ from ligo.skymap.postprocess import crossmatch as ligoskymap_crossmatch
 
 def convert_to_astropy_unit(table):
     """
+    TODO: convert this to a proper docstring
+
     Loop over the column names of the given table,
     if a column name contains a pair of square bracket,
     convert that column with the proper astropy unit if possible. 
@@ -85,3 +88,39 @@ def crossmatch_with_catalog(skymap, catalog):
     crossmatch_res = ligoskymap_crossmatch(skymap, coordinates)
 
     return crossmatch_res
+
+def filter_catalog_by_type(catalog, type):
+    return catalog[catalog["type"] == type]
+
+def convert_from_ICRS_to_healpy_convention(RA, DEC):
+    """
+    TODO: convert this to a proper docstring
+
+    Convert (RA, DEC) complying to the ICRS standard to
+    the healpy convention where \theta runs from 0 at the north
+    pole to \pi at the south pole, and $\phi$ runs from 0 to 2\pi
+
+    Input: RA, DEC
+    Output: theta, phi
+    """
+    return np.pi/2 - np.deg2rad(DEC), np.deg2rad(RA)
+
+def plot_catalog(catalog, filename="catalog.png"):
+    import healpy as hp
+    from matplotlib import pyplot as plt
+
+    # Filter catalog by type
+    galaxy_lenses = filter_catalog_by_type(catalog, "galaxy")
+    cluster_lenses = filter_catalog_by_type(catalog, "cluster")
+
+    # Fiducial value for plotting
+    _NSIDE = 32 
+    plt.figure(dpi=300)
+    # Set up a blank canvas, rotated by 180 deg so that 0 is at the left margin
+    hp.mollview(np.zeros(hp.nside2npix(_NSIDE))+4.5, rot=180, cbar=False, min=-1, max=5, bgcolor="none", cmap="Greys")
+    hp.projscatter(*convert_from_ICRS_to_healpy_convention(galaxy_lenses["RA"], galaxy_lenses["DEC"]), lonlat=False, color="cyan", s=15, label="Galaxy", alpha = 0.7)
+    hp.projscatter(*convert_from_ICRS_to_healpy_convention(cluster_lenses["RA"], cluster_lenses["DEC"]), lonlat=False, color="magenta", s=15, label="Cluster", alpha = 0.7)
+    hp.graticule(alpha=0.4, color="grey")
+    plt.legend(labelcolor="white", facecolor="dimgrey")
+    plt.title("Lenses included in "+r"${\tt lenscat}$", fontsize=18, color="white")
+    plt.savefig(filename, transparent=True)
