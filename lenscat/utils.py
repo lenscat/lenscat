f5b7272b0a4a2307499cbe5e8c2700e26cb31deb
Ka Lok (Rico) Lo <rico.kaloklo@gmail.com>
2024-02-05 14:30:27 +0100

    Update plot_catalog()

--- lenscat/utils.py
+++ lenscat/utils.py
@@ -104,15 +104,17 @@ def plot_catalog(catalog, filename="catalog.png"):
 
     # Fiducial value for plotting
     _NSIDE = 32 
-    plt.figure(dpi=300)
+    fig = plt.figure(dpi=300)
     # Set up a blank canvas, rotated by 180 deg so that 0 is at the left margin
-    hp.mollview(np.zeros(hp.nside2npix(_NSIDE))+4.5, rot=180, cbar=False, min=-1, max=5, bgcolor="none", cmap="Greys")
-    hp.projscatter(*convert_from_ICRS_to_healpy_convention(galaxy_lenses["RA"], galaxy_lenses["DEC"]), lonlat=False, color="cyan", s=15, label="Galaxy", alpha = 0.7)
-    hp.projscatter(*convert_from_ICRS_to_healpy_convention(cluster_lenses["RA"], cluster_lenses["DEC"]), lonlat=False, color="magenta", s=15, label="Cluster", alpha = 0.7)
+    hp.mollview(np.zeros(hp.nside2npix(_NSIDE)), rot=180, cbar=False, min=-1, max=5, bgcolor="none", cmap="PuRd")
+    hp.projscatter(*convert_from_ICRS_to_healpy_convention(galaxy_lenses["RA"], galaxy_lenses["DEC"]), lonlat=False, color="lightskyblue", s=15, label="Galaxy", alpha = 0.95)
+    hp.projscatter(*convert_from_ICRS_to_healpy_convention(cluster_lenses["RA"], cluster_lenses["DEC"]), lonlat=False, color="plum", s=15, label="Cluster", alpha = 0.95)
     hp.graticule(alpha=0.4, color="grey")
-    plt.legend(labelcolor="white", facecolor="dimgrey")
-    plt.title("Lenses included in "+r"${\tt lenscat}$", fontsize=18, color="white")
-    plt.savefig(filename, transparent=True)
+    plt.legend(labelcolor="black", facecolor="whitesmoke")
+    if filename is None:
+        return fig
+    else:
+        plt.savefig(filename, transparent=True)
 
 def get_precision(x):
     return 10**(decimal.Decimal(str(x)).as_tuple().exponent)
