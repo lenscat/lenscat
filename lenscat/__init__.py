76bbb10344eba407463e3322990c6cbefaedbe44
Ka Lok (Rico) Lo <rico.kaloklo@gmail.com>
2024-02-12 11:41:25 +0100

    Expose the absolute filepath to the catalog csv file

--- lenscat/__init__.py
+++ lenscat/__init__.py
@@ -5,11 +5,12 @@ from .catalog import Catalog
 
 # The relative path to the actual catalog in csv format
 _catalog_path = "data/catalog.csv"
+# The absolute path to the actual catalog in csv format
+catalog_filepath = os.path.join(
+    pathlib.Path(__file__).resolve().parent,
+    *_catalog_path.split('/')
+)
 
 # Construct a Catalog object from the csv file
-catalog = Catalog.read(
-    os.path.join(
-        pathlib.Path(__file__).resolve().parent,
-        *_catalog_path.split('/')
-    ), format="ascii.csv")
+catalog = Catalog.read(catalog_filepath, format="ascii.csv")
 
